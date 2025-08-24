4c0385c3c4db827da18f3918398fb45d39f59a45
sohail-c <sohail@chutani.com>
2025-08-24 16:27:34 -0700

    feat: improved about & contact page. Added picture, fixed a few typos, and added links to github and linkedin

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
+
 export default function About() {
   return (
     <main className="p-6 max-w-2xl mx-auto">
-    
-      <h1 className="text-4xl font-bold mb-4">About & Contact</h1>
+      <h1 className="text-center text-4xl font-bold mb-4">About & Contact</h1>
+
+      <Image
+        src="/images/about.jpg"
+        alt="A picture of me in a University of Washington Bothell purple sweatshirt"
+        width={160}
+        height={160}
+        className="w-60 h-60 rounded-full mx-auto mb-8 object-cover"
+      />
+
       <p className="mb-2">
         Hello! My name is Sohail Chutani and I&apos;m a Computer Engineering student at the University of Washington Bothell and a photographer.
       </p>
       <p className="mb-2">
-        This site is a work-in-progress project for summer 2025.
+        This site is a work-in-progress project started in earnest in late August 2025.
       </p>
       <p className="mb-2">
         I&apos;ve been meaning to create a photography portfolio for a long time now and decided to finally try to do it this summer and make it a learning experience as well.
@@ -16,7 +26,7 @@ export default function About() {
       <p className="mb-2">
         My goal is to create this site with a modern web stack. 
         My current plan is to use Next.js with the new App Router, 
-        Tailwind CSS for styling, and GitHub pages for hosting. 
+        Tailwind CSS for styling, and Vercel for deployment. 
         I also want to eventually integrate Supabase for storage my photos and possibly 
         a CMS for easier content management. 
       </p>
@@ -31,7 +41,18 @@ export default function About() {
       <p className="mb-2">
         Feel free to reach out to me at <Link href="mailto:sohail@chutani.com" className="text-blue-600 underline">sohail@chutani.com</Link>
       </p>
-      
+
+      <div className="mt-6 flex justify-center gap-6">
+        <Link href="https://github.com/sohail-c" target="_blank" className="flex items-center gap-2 text-gray-800 hover:text-black transition">
+          <Image src="/github.svg" alt="GitHub" width={24} height={24} />
+          <span className="hidden sm:inline">GitHub</span>
+        </Link>
+
+        <Link href="https://linkedin.com/in/sohailchutani" target="_blank" className="flex items-center gap-2 text-gray-800 hover:text-black transition">
+          <Image src="/linkedin.svg" alt="LinkedIn" width={24} height={24} />
+          <span className="hidden sm:inline">LinkedIn</span>
+        </Link>
+      </div>
     </main>
   );
 }
